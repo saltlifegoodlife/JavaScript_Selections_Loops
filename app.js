79b9576b3e06f0c78aff7a063f079f33a9202858
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-14 15:34:18 -0500

    added more exercises

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ for (let i = 1; i <= 100; i++) {
     console.log(`${i}: FIZZ`);
   } else if (i % 5 === 0) {
     console.log(`${i}: BUZZ`);
-  } else {
   }
 }
 
@@ -32,7 +31,6 @@ while (i <= 100) {
     console.log(`${i}: FIZZ`);
   } else if (i % 5 === 0) {
     console.log(`${i}: BUZZ`);
-  } else {
   }
   i++;
 }
@@ -53,7 +51,36 @@ do {
     console.log(`${i}: FIZZ`);
   } else if (i % 5 === 0) {
     console.log(`${i}: BUZZ`);
-  } else {
   }
   i++;
 } while (i <= 100);
+
+let value = Math.round(Math.random() * 500); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+let answer;
+for (let i = 0; i <= n; i++) {
+  if (i === value) {
+    answer = `Found value! It is: ${value}`;
+    break;
+  } else {
+    answer = "Did not find value";
+  }
+}
+console.log(answer);
+
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+
+console.log(`dividers: ${fizzDivisor} and ${buzzDivisor}`);
+
+for (i = start; i <= end; i++) {
+  if (i % fizzDivisor === 0 && i % buzzDivisor === 0) {
+    console.log(`${i}: FIZZBUZZ`);
+  } else if (i % fizzDivisor === 0) {
+    console.log(`${i}: FIZZ`);
+  } else if (i % buzzDivisor === 0) {
+    console.log(`${i}: BUZZ`);
+  }
+}
