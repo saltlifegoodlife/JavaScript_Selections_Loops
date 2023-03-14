cd6776918f70a604bd6b2d336d6beecdff31c5ca
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-14 15:17:28 -0500

    finished exercises

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
-console.log("Hello World!\n==========\n");
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  continue;
+}
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i}: FIZZBUZZ`);
+  } else if (i % 3 === 0) {
+    console.log(`${i}: FIZZ`);
+  } else if (i % 5 === 0) {
+    console.log(`${i}: BUZZ`);
+  } else {
+  }
+}
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+let i = 1;
+while (i <= 100) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
+}
+i = 1;
+
+while (i <= 100) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i}: FIZZBUZZ`);
+  } else if (i % 3 === 0) {
+    console.log(`${i}: FIZZ`);
+  } else if (i % 5 === 0) {
+    console.log(`${i}: BUZZ`);
+  } else {
+  }
+  i++;
+}
+i = 1;
+
+do {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+  i++;
+} while (i <= 100);
+i = 1;
+
+do {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i}: FIZZBUZZ`);
+  } else if (i % 3 === 0) {
+    console.log(`${i}: FIZZ`);
+  } else if (i % 5 === 0) {
+    console.log(`${i}: BUZZ`);
+  } else {
+  }
+  i++;
+} while (i <= 100);
